168274b80c9191b7e8026329a69a6504475232bb
Darren Anderson <dranders@colorado.edu>
2017-04-02 15:56:30 -0700

    save section and entry definitions to JSON

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
 require('dotenv').config();
 const program = require('commander');
 const debug = require('debug')('index');
+const path = require('path');
+const fs = require('fs-extra');
 const messages = require('./messages');
 const tp = require('./template_parser');
+// constants
+const OUTPUT_DATA_DIR = path.join(__dirname, 'output_data');
+
 // command line setup
 program
   .version('0.1.0');
@@ -20,7 +25,10 @@ function main() {
   debug('starting');
   try {
     const data = tp.parse();
-    debug(data);
+    fs.outputJsonSync(path.join(OUTPUT_DATA_DIR, 'section_def.nation.json'), data.sections.nation);
+    fs.outputJsonSync(path.join(OUTPUT_DATA_DIR, 'section_def.jurisdiction.json'), data.sections.jurisdiction);
+    fs.outputJsonSync(path.join(OUTPUT_DATA_DIR, 'entry_def.scalar.json'), data.entries.scalars);
+    fs.outputJsonSync(path.join(OUTPUT_DATA_DIR, 'entry_def.collection.json'), data.entries.collections);
   } catch (err) {
     debug(err);
     debug('quitting');
